81d96085639f325c0c11eca4c3ab188845f1acb8
Josh Rockwood <joshrockwood@hotmail.com>
2023-10-26 14:05:11 -0600

    AnimationController Script

---
+++ Unity/Side Scroller Prototype/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public Animator animator;
+    private bool isJumping;
+    private readonly int idle = Animator.StringToHash("Idle");
+    private readonly int walk = Animator.StringToHash("Walk");
+    private readonly int jump = Animator.StringToHash("Jump");
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+        HandleJumpToIdle();    
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            animator.SetTrigger(jump);
+            isJumping = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            animator.SetTrigger(idle);
+        }
+    }
+
+    private void HandleJumpToIdle()
+    {
+        if (!isJumping || !animator.GetCurrentAnimatorStateInfo(0).IsName("Idle")) return;
+        animator.SetTrigger(idle);
+        isJumping = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetTrigger(idle);
+    }
+}
