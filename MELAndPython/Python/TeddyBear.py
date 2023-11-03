d44caeed5518fafaa3ce7893364b3d8927c0b211
Josh Rockwood <joshrockwood@hotmail.com>
2023-11-02 22:24:35 -0600

    Finished TeddyBear script

--- MELAndPython/Python/TeddyBear.py
+++ MELAndPython/Python/TeddyBear.py
@@ -84,3 +84,119 @@ cmds.move(-1.324341, 0, 0, relative=True)
 cmds.move(0.0113544, 0, 0, relative=True)
 
 # created mouth
+cmds.polySphere(radius=1, subdivisionsX=20, subdivisionsY=20, axis=(0, 1, 0), createUVs=2, constructionHistory=True)
+# Result: pSphere3 polySphere3
+cmds.move(0, 2.65422, 0, relative=True)
+cmds.scale(0.544073, 0.544073, 0.544073, relative=True)
+cmds.move(0, 0, 0.570157, relative=True)
+cmds.scale(0.639928, 0.639928, 0.639928, relative=True)
+cmds.scale(1.287205, 1, 1, relative=True)
+cmds.move(0, 0, 0.080862, relative=True)
+cmds.move(0, 0.0423663, 0, relative=True)
+cmds.move(0, 0, -0.0036949, relative=True)
+cmds.move(0.0202071, 0, 0, relative=True)
+cmds.move(0, 0, 0.0335526, relative=True)
+
+# created nose
+cmds.polySphere(radius=1, subdivisionsX=20, subdivisionsY=20, axis=(0, 1, 0), createUVs=2, constructionHistory=True)
+# Result: pSphere4 polySphere4
+cmds.move(0, 3.016181, 0, relative=True)
+cmds.scale(0.300128, 0.300128, 0.300128, relative=True)
+cmds.move(0, 0, 0.964937, relative=True)
+cmds.scale(1, 0.520162, 1, relative=True)
+cmds.scale(1, 1, 0.393965, relative=True)
+cmds.scale(0.586746, 0.586746, 0.586746, relative=True)
+cmds.move(0, -0.0902506, 0, relative=True)
+cmds.move(0, 0, -0.0519513, relative=True)
+cmds.move(0.0131164, 0, 0, relative=True)
+
+# created left eye
+cmds.polySphere(radius=1, subdivisionsX=20, subdivisionsY=20, axis=(0, 1, 0), createUVs=2, constructionHistory=True)
+# Result: pSphere5 polySphere5
+cmds.move(0, 2.875458, 0, relative=True)
+cmds.scale(0.248146, 0.248146, 0.248146, relative=True)
+cmds.move(0, 0, 1.01145, relative=True)
+cmds.move(0.448271, 0, 0, relative=True)
+cmds.move(0, 0.219047, 0, relative=True)
+cmds.scale(0.505108, 0.505108, 0.505108, relative=True)
+cmds.move(0, 0, -0.478681, relative=True)
+cmds.move(-0.114673, 0, 0, relative=True)
+cmds.move(0, 0, 0.0307127, relative=True)
+cmds.move(0, 0, 0.0528253, relative=True)
+cmds.move(-0.0215712, 0, 0, relative=True)
+cmds.select('pSphere5', replace=True)
+cmds.select('pSphere5.f[0:159]', 'pSphere5.f[360:379]', replace=True)
+cmds.scale(1, 0.400299, 1, pivot=(0.312026, 3.022032, 0.616307), relative=True)
+
+# created right eye
+cmds.select('pSphere5', replace=True)
+cmds.duplicate(returnRootsOnly=True)
+# Result: pSphere6
+cmds.move(-0.601166, 0, 0, relative=True)
+cmds.move(0.0178893, 0, 0, relative=True)
+
+# created left arm
+cmds.polyCylinder(radius=1, height=2, subdivisionsX=20, subdivisionsY=1, subdivisionsZ=1, axis=(0, 1, 0), roundCap=0, createUVs=3, constructionHistory=True)
+# Result: pCylinder6 polyCylinder4
+cmds.move(0, 2.754125, 0, relative=True)
+cmds.rotate(82.721166, 0, 0, relative=True, objectSpace=True, forceOrderXYZ=True)
+cmds.rotate(19.243704, 0, 0, relative=True, objectSpace=True, forceOrderXYZ=True)
+cmds.scale(0.341187, 0.341187, 0.341187, relative=True)
+cmds.scale(1, 2.306521, 1, relative=True)
+cmds.move(0, 0, 1.33867, relative=True)
+cmds.move(1.385834, 0, 0, relative=True)
+cmds.rotate(0, 0, -37.262805, relative=True, objectSpace=True, forceOrderXYZ=True)
+cmds.move(0, -0.81743, 0, relative=True)
+cmds.move(-0.220002, 0, 0, relative=True)
+cmds.move(0, 0, -0.363331, relative=True)
+cmds.move(0, -0.386327, 0, relative=True)
+cmds.move(0 ,0, -0.460281, relative=True)
+cmds.move(-0.373012, 0, 0, relative=True)
+cmds.move(0, 0.226875, 0, relative=True)
+cmds.move(0, 0, -0.218735, relative=True)
+cmds.move(-0.134022, 0, 0, relative=True)
+
+# created right arm
+cmds.select('pCylinder5', replace=True)
+cmds.duplicate(returnRootsOnly=True)
+# Result: pCylinder7
+cmds.move(-1.580796, 0, 0, relative=True)
+cmds.rotate(0, 0, 70.915572, relative=True, objectSpace=True, forceOrderXYZ=True)
+cmds.move(0.320385, 0, 0, relative=True)
+cmds.move(-0.0877784, 0, 0, relative=True)
+cmds.rotate(0, 0, 4.224965, relative=True, objectSpace=True, forceOrderXYZ=True)
+cmds.move(0.022435, 0, 0, relative=True)
+
+# created bowtie
+cmds.polySphere(radius=1, subdivisionsX=20, subdivisionsY=20, axis=(0, 1, 0), createUVs=2, constructionHistory=True)
+# Result: pSphere7 polySphere6
+cmds.move(0, 2.078457, 0, relative=True)
+cmds.move(0, 0, 1.05925, relative=True)
+cmds.scale(0.183676, 0.183676, 0.183676, relative=True)
+cmds.scale(1, 1, 0.540182, relative=True)
+cmds.scale(1, 0.785058, 1, relative=True)
+cmds.scale(1, 1, 0.696713, relative=True)
+cmds.move(0, 0, -0.226482, relative=True)
+cmds.rotate(-17.958833, 0, 0, relative=True, objectSpace=True, forceOrderXYZ=True)
+cmds.move(0, 0, -0.105057, relative=True)
+cmds.move(0, 0.0633034, 0, relative=True)
+cmds.polyCone(radius=1, height=2, subdivisionsX=20, subdivisionsY=1, subdivisionsZ=0, axis=(0, 1, 0), roundCap=0, createUVs=3, constructionHistory=True)
+# Result: pCone1 polyCone1
+cmds.move(0, 0, 1.104295, relative=True)
+cmds.move(0, 2.178049, 0, relative=True)
+cmds.scale(0.21876, 0.21876, 0.21876, relative=True)
+cmds.scale(1, 1, 0.27989, relative=True)
+cmds.scale(1, 0.69034, 1, relative=True)
+cmds.rotate(0, 0, 92.474724, relative=True, objectSpace=True, forceOrderXYZ=True)
+cmds.move(0, 0, -0.381433, relative=True)
+cmds.move(0.241864, 0, 0, relative=True)
+cmds.rotate(0, 20.372155, 0, relative=True, objectSpace=True, forceOrderXYZ=True)
+# Undo: rotate 0, 20.372155, 0, relative, objectSpace, forceOrderXYZ
+# Redo: rotate 0, 20.372155, 0, relative, objectSpace, forceOrderXYZ
+cmds.move(0, -0.0488271, 0, relative=True)
+cmds.rotate(5.555492, 0, 0, relative=True, objectSpace=True, forceOrderXYZ=True)
+cmds.rotate(0, 6.619776, 0, relative=True, objectSpace=True, forceOrderXYZ=True)
+cmds.duplicate(returnRootsOnly=True)
+# Result: pCone2
+cmds.move(-0.478944, 0, 0, relative=True)
+cmds.rotate(0, 0, 182.513325, relative=True, objectSpace=True, forceOrderXYZ=True)
