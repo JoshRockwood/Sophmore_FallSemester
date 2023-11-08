84637649f0ee78cefb116208b29fcc1c2f6b2183
Josh Rockwood <joshrockwood@hotmail.com>
2023-11-07 20:14:33 -0700

    Finished first 4  pluralsight modules

--- MELAndPython/Python/Pluralsight_Practice.py
+++ MELAndPython/Python/Pluralsight_Practice.py
@@ -43,6 +43,45 @@ else:
 roll = random.randint(1, 6)
 guess = int(input('Guess the dice roll:\n'))
 if guess == roll:
-    print('Correct! They rolled a ' + str(roll))
+    print('Correct! They rolled a ' + str(roll) + '\n')
 else:
-    print('Wrong! They rolled a ' + str(roll))
+    print('Wrong! They rolled a ' + str(roll) + '\n')
+
+# Expenses
+expenses = [10.50, 8, 5, 15, 20, 5, 3]
+total = sum(expenses)
+print('You spent $', total, sep = '')
+
+# User input for expenses
+total = 0
+expenses =[]
+num_expenses = int(input("Enter # of expenses:"))
+for i in range(num_expenses):
+    expenses.append((float(input("Enter an expense:"))))
+total = sum(expenses)
+print("You spent $", total, sep = '')
+
+#Loan calculator
+# Get details of loan
+money_owed = float(input("How much money do you owe, in dollars?\n")) #50,000
+apr = float(input('What is the annual percentage rate of the loan?\n')) #3%
+payment = float(input('How much will you pay off each month in dollars?\n')) #1,000
+months = int(input('How many months do you want to see the results for?\n')) #24
+
+montly_rate = apr/100/12
+
+for i in range(months):
+    # Calculate interest to pay
+    interest_paid = money_owed * montly_rate
+    #Add in interest
+    money_owed = money_owed + interest_paid
+
+    if (money_owed - payment < 0):
+        print('The last payment is', money_owed)
+        print('You paid off the loan in', i + 1, 'months')
+        break
+    #Make payment
+    money_owed = money_owed - payment
+
+    print('Paid $', payment, 'of which $', interest_paid, 'was interest', end = ' ')
+    print('Now I owe $', money_owed)
