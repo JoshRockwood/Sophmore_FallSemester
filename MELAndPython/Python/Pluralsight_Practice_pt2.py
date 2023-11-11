ef4a9d1f0e50f6b01c5578d565cedafecf24caa7
Josh Rockwood <joshrockwood@hotmail.com>
2023-11-10 20:20:10 -0700

    Pluralsight Practice

--- MELAndPython/Python/Pluralsight_Practice_pt2.py
+++ MELAndPython/Python/Pluralsight_Practice_pt2.py
@@ -1,3 +1,4 @@
+import random
 # Movie schedule
 
 current_movies = {'The Grinch': '11:00am',
@@ -20,15 +21,43 @@ else:
 
 # Dictionary of contacts
 contacts = {
-            'number': 4,
-            'students':
-              [
-                  {'name': 'Sarah Holderness', 'email': 'sarah@example.com'},
-                  {'name': 'Harry Potter', 'email': 'harry@example.com'},
-                  {'name': 'Hermione Granger', 'email': 'hermione@example.com'},
-                  {'name': 'Ron Weasley', 'email': 'ron@example.com'}
-              ]
-          }
+    'number': 4,
+    'students':
+        [
+            {'name': 'Sarah Holderness', 'email': 'sarah@example.com'},
+            {'name': 'Harry Potter', 'email': 'harry@example.com'},
+            {'name': 'Hermione Granger', 'email': 'hermione@example.com'},
+            {'name': 'Ron Weasley', 'email': 'ron@example.com'}
+        ]
+}
 print('Student emails:')
 for student in contacts['students']:
     print(student['email'])
+
+# Dice rolling game
+
+
+def roll_dice():
+    dice_total = random.randint(1, 6) + random.randint(1, 6)
+    return dice_total
+
+
+def main():
+    player1 = input("Enter player 1's name:\n")
+    player2 = input("Enter player 2's name:\n")
+
+    roll1 = roll_dice()
+    roll2 = roll_dice()
+
+    print(player1, 'rolled', roll2)
+    print(player2, 'rolled', roll2)
+
+    if roll1 > roll2:
+        print(player1, "Wins")
+    elif roll2 > roll1:
+        print(player2, "Wins")
+    else:
+        print("Tie game")
+
+
+main()
