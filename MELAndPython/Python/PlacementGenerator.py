2b3ba4d7aa32801f9636cb036a349223f4d9abba
Josh Rockwood <joshrockwood@hotmail.com>
2023-11-13 12:04:04 -0700

    Updated placement generator

--- MELAndPython/Python/PlacementGenerator.py
+++ MELAndPython/Python/PlacementGenerator.py
@@ -2,22 +2,22 @@ import maya.cmds as cmds
 import random
 
 
-def placement_generator(x_min, x_max, y_max, y_min, z_max, z_min, numb_dups):
-    sels = [cmds.ls(selection=True)]
+def placement_generator(x_min, x_max, y_min, y_max, z_min, z_max, numb_dups):
+    sels = cmds.ls(selection=True)
 
-    if sels == 0:
-        print('Make a selection before running PlacementGenerator()')
+    if len(sels) < 1:
+        cmds.error('Please make a selection.')
 
     for i in range(len(sels)):
         for j in range(numb_dups):
-            dups = [cmds.duplicate(sels[i], returnRootsOnly=True)]
+            dups = cmds.duplicate(sels[i], returnRootsOnly=True)
             dup = dups[0]
 
-            rand_x = random.randint(x_min, x_max)
-            rand_y = random.randint(y_min, y_max)
-            rand_z = random.randint(z_min, z_max)
+            rand_x = random.randrange(x_min, x_max)
+            rand_y = random.randrange(y_min, y_max)
+            rand_z = random.randrange(z_min, z_max)
 
-            cmds.xform(dup, worldSpace=True, translation=(rand_x, rand_y, rand_z))
+            cmds.xform(dup, worldSpace=True, translation=[rand_x, rand_y, rand_z])
 
 
 placement_generator(10, 30, 0, 1, 10, 30, 10)
