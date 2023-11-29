86f51702d1419d9416cda20205a76738a3f51ebe
josh_rockwood <joshrockwood@hotmail.com>
2023-11-28 23:58:58 -0700

    DesktopUpdates

--- Unity/SideScrollerPrototype/Assets/Scripts/AnimationController.cs
+++ Unity/SideScrollerPrototype/Assets/Scripts/AnimationController.cs
@@ -7,12 +7,13 @@ public class AnimationController : MonoBehaviour
     private readonly int idle = Animator.StringToHash("Idle");
     private readonly int walk = Animator.StringToHash("Walk");
     private readonly int jump = Animator.StringToHash("Jump");
+    private readonly int run = Animator.StringToHash("Run");
 
     // Update is called once per frame
     void Update()
     {
         HandleInput();
-        HandleJumpToIdle();    
+        HandleJumpToIdle(); 
     }
 
     private void HandleInput()
@@ -22,11 +23,19 @@ public class AnimationController : MonoBehaviour
             animator.SetTrigger(jump);
             isJumping = true;
         }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
         {
             animator.SetTrigger(walk);
         }
-        else if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        else if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetTrigger(idle);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift) && !isJumping)
+        {
+            animator.SetTrigger(run);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
         {
             animator.SetTrigger(idle);
         }
